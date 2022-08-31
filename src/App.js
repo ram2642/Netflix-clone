d1b3856b7ea902600dc36ef7df44a22c586433b9
ramlakhan singh <ramlakhan.singh.cs.2019@mitmeerut.ac.in>
2022-08-31 06:50:47 +0530

    first build'

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import HomeScreen from './HomeScreen';
+
+function App() {
+  return (
+    <div className="app">
+     <HomeScreen/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMA1JS7GZ428j5GE6ntCIc7lsB57uJgrY",
+  authDomain: "netfix-clone-d1233.firebaseapp.com",
+  projectId: "netfix-clone-d1233",
+  storageBucket: "netfix-clone-d1233.appspot.com",
+  messagingSenderId: "577749491359",
+  appId: "1:577749491359:web:a08434bf94ac74d7b0ccaa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
